80f5ba86592cb503c4ca1da24359782a30124040
hilariodantas <hilariod94@gmail.com>
2022-12-20 21:38:35 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,64 @@
 import useSWR from 'swr'
 import {useState} from 'react'
+import { useForm } from 'react-hook-form'
 
-export function TheForm(){
+export function TheForm({filtro, url, acao}){
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     return (
-
         <div>
+            <style jsx>{`
+                .form-search-movie {
+                    display: flex;
+                    flex-direction: column;
+                    max-width: 200px;
+                    gap: 10px;
+                }
+                .form-search-movie input {
+                    padding: 5px;
+                    border-radius: 5px;
+                    border: 1px solid #000;
+                    outline: none;
+                }
+                .form-search-movie button {
+                    background-color: #0080FF;
+                    color: #fff;
+                    padding: 5px;
+                    border: none;
+                    border-radius: 5px;
+                    margin-bottom: 10px;
+                    cursor: pointer;
+                }
+                
+            `}</style>
+            <form className='form-search-movie' onSubmit={handleSubmit(acao)}>
+                <label htmlFor="titleSearchString">{filtro}</label>
 
-            <form>
+                <input 
+                    id="titleSearchString" 
+                    {...register("titleSearchString", { required: true, minLength: 3})} 
+                    type="text" 
+                    autoComplete="true"
+                    placeholder='Título'
+                />
 
-                <label htmlFor="titleSearchString">Filtro de Título</label>
+                {errors.titleSearchString && <span>Insira no mínimo três caracteres   </span>}
 
-                <input id="titleSearchString" name="titleSearchString" type="text" autoComplete="true"/>
+                <input 
+                    id="yearSearchNumber" 
+                    {...register("yearSearchNumber", { required: true, minLength: 4, maxLength: 4 })} 
+                    type="text" 
+                    autoComplete="true"
+                    placeholder='Ano'
+                />
 
+                {errors.yearSearchNumber && <span> Insira um ano válido </span> }
+                <button type='submit'>{url === '' ? 'Mostrar' : 'Ocultar'}</button>
+                
             </form>
-
         </div>
-
     )
-
 }
 
 export function TheMovies({data,show}){
@@ -29,86 +69,53 @@ export function TheMovies({data,show}){
 
     if (data.error) return (<div>falha na pesquisa</div>)
 
-    if (data.Search === '' ) return (<div>carregando...</div>)
+    if (data.Response == 'False') return (<div>Não foi encontrado nenhum filme com essas informações </div>)
 
     return (
 
         <div>
-
-            { data.Search.map( (m) => <div key={m.imdbID}>{m.Title} --- {m.Year}</div>  ) }            
-
+            <div> {data.Title} --- {data.Year}</div>        
         </div>
-
     )
-
-}
-
-
-
-export function TheLink({url, handler}){
-
-    return (
-
-        <div>
-
-            <a href="/movies3.js" onClick={handler}> {url === '' ? 'Mostrar' : 'Ocultar'} </a>
-
-        </div>
-
-    )
-
+    
 }
 
 export default function Movies33(){
 
-    const [state, setState] = useState({url:'', titleSearchString:''})
+    const [state, setState] = useState({url:'', titleSearchString:'', yearSearchNumber:''});
 
-    const {data, error} = useSWR(state.url, async (u) => {
+    const {data, error} = useSWR(state.url, async () => {
+        
+        if (!state.url || !state.titleSearchString || !state.yearSearchNumber) return {Search:''}
+        if (state.url === '' || state.titleSearchString ==='' || state.yearSearchNumber === '') return {Search:''}
 
-        if (!state.url || !state.titleSearchString) return {Search:''}
-
-        if (state.url === '' || state.titleSearchString ==='') return {Search:''}
-
-        const res = await fetch(`${state.url}/?apiKey=678e6ae&s=${state.titleSearchString}`)
-
+        const res = await fetch(`${state.url}/?apiKey=678e6ae&t=${state.titleSearchString}&y=${state.yearSearchNumber}`)
         const json = await res.json();
 
-        return json;
+        return json
 
     })
 
-    const onClickHandler = e => {
 
-        e.preventDefault()
-
-        let s = document.getElementById('titleSearchString').value
+    const onClickHandler = () => {
+        
+        const t = document.querySelector('#titleSearchString').value
+        const y = document.querySelector('#yearSearchNumber').value
 
         if (state.url === '') {
-
-            setState({url:'http://www.omdbapi.com',titleSearchString:s})
-
+            setState({url:'https://www.omdbapi.com',titleSearchString:t,yearSearchNumber:y})
         }
-
-        else setState({url:'',titleSearchString: state.titleSearchString})
-
+        else{
+            setState({url:'',titleSearchString: state.titleSearchString, yearSearchNumber: state.yearSearchNumber})
+        }
     }
 
 
 
     return (
-
         <div>
-
-            <TheForm/>
-
-            <TheLink url={state.url} handler={onClickHandler} />
-
-            <TheMovies data={data ? data: {Search:''} } show={state.url !== ''} />
-
-        
-
+            <TheForm filtro={'Filtro de título'} url={state.url} acao={onClickHandler}/>
+            <TheMovies data={error?{error:'Erro na pesquisa'}: data ? data: {Search:''} } show={state.url !== ''} />
         </div>
-
     )
-
 }
